d0bcb19b3892746e8dd65279e222988cf85a17e7
mateus lavezo <ssmokeagain@gmail.com>
2018-09-26 17:25:10 -0300

    exercicio5publicandoprojetos
    publique pelo menos 2 exercicios da materia PA

---
+++ Ex1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Maiores");
+            Console.WriteLine("---------");
+
+            int n = 0;
+            int qtdeMaiores69 = 0;
+
+            int i = 1;
+            while (i <= 6)
+            {
+                Console.Write("Digite o " + i + "º numero: ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                if (n > 69)
+                {
+                    qtdeMaiores69++;
+                }
+                i++;
+            }
+            Console.WriteLine("Qtde de maiores que 69: " + qtdeMaiores69);
+            Console.ReadKey();
+        }
+    }
+}
